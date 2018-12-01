99ad7ed78b9930933c5f5f069558ec5180b9f2b4
Katharina Peter <gaz@gmx.net>
2018-12-01 20:45:40 +0100

    Add styling to forms

---
+++ app/lib/bootstrap_form_builder.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+# form builder wrapper that adds bootstrap classes
+class BootstrapFormBuilder < ActionView::Helpers::FormBuilder
+  def label(method, text = nil, options = {}, &block)
+    super(method, text, insert_class('control-label', options), &block)
+  end
+
+  def text_field(method, options = {})
+    super(method, insert_class('form-control', options))
+  end
+
+  def submit(value = nil, options = {})
+    super(value, insert_class('btn btn-primary', options))
+  end
+
+  def group(method, &block)
+    class_names = if object.errors.key?(method)
+                    'form-group has-error'
+                  else
+                    'form-group'
+                  end
+
+    content = @template.capture(&block)
+
+    %(<div class="#{class_names}">#{content}</div>).html_safe
+  end
+
+  private
+
+  def insert_class(class_name, options)
+    output = options.dup
+    output[:class] = ((output[:class] || '') + " #{class_name}").strip
+    output
+  end
+end
